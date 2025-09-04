a1ec24a5be3a505998added4bc33434d882204aa
Chunji Wang <chunji.wang@samsung.com>
2025-09-03 17:14:28 -0700

    Train movielens matrix factorization model

--- notebooks/movielens.py
+++ notebooks/movielens.py
@@ -12,18 +12,25 @@ def _():
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""## MovieLens Dataset""")
+    mo.md(
+        r"""
+    Largely follows https://github.com/yanneta/pytorch-tutorials/blob/master/collaborative-filtering-nn.ipynb
+    ## MovieLens Dataset
+    """
+    )
     return
 
 
 @app.cell
 def _():
+    import numpy as np
+    import pandas as pd
+    from pandas import DataFrame
+    pd.options.mode.copy_on_write = True
+
     from pathlib import Path
     from collections import defaultdict
-    import pandas as pd
-    import numpy as np
-    pd.options.mode.copy_on_write = True
-    return Path, defaultdict, np, pd
+    return DataFrame, Path, defaultdict, np, pd
 
 
 @app.cell
@@ -81,7 +88,7 @@ def _(defaultdict, pd):
 
 
 @app.cell
-def _(defaultdict, pd):
+def _(DataFrame, defaultdict):
     class IdEncoder:
         """Sklearn-like encoder for userId/movieId integer mapping."""
 
@@ -89,21 +96,21 @@ def _(defaultdict, pd):
             self.mappings = {}
             self.num_uniques = {}
 
-        def fit(self, df: pd.DataFrame, cols=("userId", "movieId")):
+        def fit(self, df: DataFrame, cols=("userId", "movieId")):
             for col in cols:
                 uniq = df[col].unique()
                 self.num_uniques[col] = len(uniq)
                 self.mappings[col] = defaultdict(lambda:-1, {o: i for i, o in enumerate(uniq)})
             return self
 
-        def transform(self, df: pd.DataFrame, cols=("userId", "movieId")) -> pd.DataFrame:
+        def transform(self, df: DataFrame, cols=("userId", "movieId")) -> DataFrame:
             for col in cols:
                 mapping = self.mappings[col]
                 df[col] = df[col].map(mapping)
                 df = df[df[col] != -1]
             return df
 
-        def fit_transform(self, df: pd.DataFrame, cols=("userId", "movieId")) -> pd.DataFrame:
+        def fit_transform(self, df: DataFrame, cols=("userId", "movieId")) -> DataFrame:
             return self.fit(df, cols).transform(df, cols)
     return (IdEncoder,)
 
@@ -138,8 +145,8 @@ def _(mo):
 def _():
     import torch
     import torch.nn as nn
-    import torch.nn.functional as fl
-    return nn, torch
+    import torch.nn.functional as F
+    return F, nn, torch
 
 
 @app.cell
@@ -155,8 +162,111 @@ def _(embed, torch):
     return
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Matrix factorization model""")
+    return
+
+
 @app.cell
-def _():
+def _(nn):
+    class MF(nn.Module):
+        def __init__(self, num_users, num_items, emb_size=100):
+            super(MF, self).__init__()
+            self.user_emb = nn.Embedding(num_users, emb_size)
+            self.item_emb = nn.Embedding(num_items, emb_size)
+            self.user_emb.weight.data.uniform_(0, 0.05)
+            self.item_emb.weight.data.uniform_(0, 0.05)
+        
+        def forward(self, u, v):
+            u = self.user_emb(u)
+            v = self.item_emb(v)
+            return (u*v).sum(1)
+    return (MF,)
+
+
+@app.cell
+def _(df_train):
+    df_train['userId'].values
+    return
+
+
+@app.cell
+def _(df_train, nn, torch):
+    num_users = df_train['userId'].max() + 1
+    num_items = df_train['movieId'].max() + 1
+    emb_size = 30
+
+    user_emb = nn.Embedding(num_users, emb_size)
+    item_emb = nn.Embedding(num_items, emb_size)
+    users = torch.LongTensor(df_train['userId'].values.copy())
+    items = torch.LongTensor(df_train['movieId'].values.copy())
+    return item_emb, items, num_items, num_users, user_emb, users
+
+
+@app.cell
+def _(item_emb, items, user_emb, users):
+    U = user_emb(users)
+    V = item_emb(items)
+    (U*V).sum(1)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Prepare data epochs""")
+    return
+
+
+@app.cell
+def _(df_train, torch):
+    from torch.utils.data import TensorDataset, DataLoader
+
+    user_tensor = torch.LongTensor(df_train['userId'].values.copy())
+    item_tensor = torch.LongTensor(df_train.movieId.values.copy())
+    rating_tensor = torch.FloatTensor(df_train.rating.values.copy())
+
+    batch_size = 1024  # or smaller/larger depending on memory
+
+    train_dataset = TensorDataset(user_tensor, item_tensor, rating_tensor)
+    train_loader  = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    return (train_loader,)
+
+
+@app.cell
+def _(MF, num_items, num_users):
+    model = MF(num_users, num_items)
+    return (model,)
+
+
+@app.cell
+def _(F, torch):
+    def train_epochs(model, train_loader, epochs=10, lr=0.01, wd=0.0):
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=wd)
+        model.train()
+    
+        for epoch in range(epochs):
+            total_loss = 0
+            for users, items, ratings in train_loader:
+            
+                preds = model(users, items)
+                loss  = F.mse_loss(preds, ratings)
+            
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            
+                total_loss += loss.item() * users.size(0)
+        
+            avg_loss = total_loss / len(train_loader.dataset)
+            print(f"Epoch {epoch+1}/{epochs}, Avg Loss: {avg_loss:.4f}")
+    return (train_epochs,)
+
+
+@app.cell
+def _(model, train_epochs, train_loader):
+    # Train for 10 epochs, learning rate 0.01, no weight decay
+    train_epochs(model, train_loader, epochs=10, lr=0.01, wd=0.0)
     return
 
 
